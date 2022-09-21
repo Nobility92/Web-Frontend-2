cdffe6bcfe05cd30fe79b7ab5c1081f58250cec9
Nobility92 <nwa21004@byui.edu>
2022-09-21 21:41:15 +0100

    c

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 const question = "What is your name";
 const answer = prompt(question);
 alert(`Your name is ${answer}`);
+
+const department = [];
+department[0] = 'Human Resource';
+department[1] = 'Marketing';
+department[2] = 'I.T';
+department[3] = 'Finance';
+
+alert(department);
